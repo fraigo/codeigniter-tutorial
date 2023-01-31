a2fa7cabf1f14922710b24db58c0d676a166921d
Francisco <franciscoigor@gmail.com>
2023-01-31 00:58:54 -0800

    Add Element properties plugin html editor

--- app/Views/components/html-editor.php
+++ app/Views/components/html-editor.php
@@ -1,4 +1,10 @@
+<?php
+    $images = [
+        // [ "title" => "Frank" , "value" => "/images/frank.jpg"]
+    ];
+?>
 <script src="/components/tinymce/tinymce.min.js" ></script>
+<input type="file" id="htmleditorfile" accept="image/*" style="display: none">
 <textarea name="<?= $name ?>" id="<?= $id ?>" style="opacity:0;width:100%;height:600px" contenteditable><?= $value ?></textarea>
 <script>
     var editor = tinymce.init({
@@ -6,7 +12,7 @@
         body_class: "some_class_name",
         menubar: false,
         // https://www.tiny.cloud/docs/tinymce/6/available-toolbar-buttons/
-        toolbar: "undo redo | bold italic | headings fontfamily fontsize | alignleft aligncenter alignright | forecolor backcolor removeformat | bullist numlist | link image | template codesample ",
+        toolbar: "undo redo | bold italic | h1 h2 h3 h4 | fontfamily | alignleft aligncenter alignright | forecolor backcolor | bullist numlist | link image table | fontsize template codesample ",
         toolbar_groups: {
             headings: {
                 text: 'Headings',
@@ -14,8 +20,56 @@
                 items: 'h1 h2 h3 h4'
             }
         },
+        relative_urls : false,
+        remove_script_host : true,
+        image_list: <?php echo json_encode($images) ?>,
+        file_picker_types: 'image',
+        file_picker_callback: (callback, value, meta) => {
+            // Provide file and text for the link dialog
+            var fileInput = document.getElementById('htmleditorfile');
+            
+            /*
+            if (meta.filetype == 'file') {
+                tinymce.activeEditor.windowManager.openUrl({
+                    title: 'Select Local Links',
+                    url: '/dialogs/linkselector',
+                    onMessage: function(api, details){
+                        console.log('message', details)
+                        if (details.mceAction=='customAction' && details.data && details.data.url){
+                            callback(details.data.url, { text: details.data.text || '' });
+                        }
+                        api.close()
+                    }
+                });
+            }
+            */
+
+            // Provide image and alt text for the image dialog
+            if (meta.filetype == 'image') {
+                fileInput.setAttribute("accept","image/*")
+                fileInput.onchange = function(e){
+                    var file = fileInput.files[0]
+                    if (file){
+                        console.log(file)
+                        var reader = new FileReader()
+                        reader.addEventListener("load", () => {
+                            callback(reader.result, { alt: file.name });
+                        }, false);
+                        reader.readAsDataURL(file);
+                    }
+                }
+                fileInput.click()
+            }
+
+            /* media
+            if (meta.filetype == 'media') {
+                //callback('movie.mp4', { source2: 'alt.ogg', poster: 'image.jpg' });
+            }
+            */
+        },
+        contextmenu: 'link image table element',
         font_family_formats: "Normal=; Times=Cambria,Times,sans-serif; Mono=Courier New,courier,monospace",
-        plugins: ["advlist", "anchor", "autolink", "codesample", "fullscreen", "help", "image", "lists", "link", "media", "preview", "searchreplace", "table", "template", "visualblocks", "wordcount"],
+        plugins: ["advlist", "anchor", "autolink", "codesample", "fullscreen", "help", "image", "lists", "link", "media", "preview", "searchreplace", "table", "template", "visualblocks", "wordcount", "element"],
         templates: [{
             "title": "2 Columns",
             "description": "",
@@ -26,6 +80,18 @@
             "content": "<div class=\"columns3 row\"><div class=\"col-12 col-md-4\">Column1</div><div class=\"col-12 col-md-4\">Column2</div><div class=\"col-12 col-md-4\">Column3</div></div><br>"
         }],
         statusbar: false,
+        setup: function(editor) {
+            editor.on('focus', function(e) {
+                window.sel = e.target.selection
+                console.log('focus', e.target.selection)
+                $(e.target.container).find('.mce-edit-area').toggleClass('focused');
+            });
+
+            editor.on('blur', function(e) {
+                console.log('blur', e.target.container)
+                $(e.target.container).find('.mce-edit-area').toggleClass('focused');
+            });
+        },
         content_css: ['/css/bootstrap.min.css','/css/style.css']
     });
 </script>
