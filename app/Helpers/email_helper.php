23738e888460b2faaf7e745e2a5456569cb2d0ce
Francisco <franciscoigor@gmail.com>
2023-02-11 22:55:42 -0800

    email config inline

--- app/Helpers/email_helper.php
+++ app/Helpers/email_helper.php
@@ -2,7 +2,13 @@
 
 function send_email($to, $subject, $view, $data=[]){
     $email = \Config\Services::email();
-    $email->setFrom('admin@'.$_SERVER['SERVER_NAME'], 'Admin');
+    $config=[];
+    $config['protocol'] = 'sendmail';
+    $config['mailPath'] = '/usr/sbin/sendmail';
+    $config['mailType']  = 'html';
+    $config['wordWrap'] = true;
+    $email->initialize($config);
+    $email->setFrom('admin@examle.com', 'Admin');
     $email->setTo($to);
     $email->setSubject($subject);
     $email->setMessage(view($view,$data));
