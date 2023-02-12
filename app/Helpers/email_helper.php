c1a1c3d6d305701b5893e6eb4d29656ed691178e
Francisco <franciscoigor@gmail.com>
2023-02-11 19:29:48 -0800

    Email config default

--- app/Helpers/email_helper.php
+++ app/Helpers/email_helper.php
@@ -2,7 +2,7 @@
 
 function send_email($to, $subject, $view, $data=[]){
     $email = \Config\Services::email();
-    $email->setFrom('admin@examle.com', 'Admin');
+    $email->setFrom('admin@'.$_SERVER['SERVER_NAME'], 'Admin');
     $email->setTo($to);
     $email->setSubject($subject);
     $email->setMessage(view($view,$data));
