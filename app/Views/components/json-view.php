6fd89b520ab9d4f3fd80608f0feba5fd4624b4d9
Francisco <franciscoigor@gmail.com>
2023-05-16 11:35:01 -0700

    Json view

---
+++ app/Views/components/json-view.php
@@ -0,0 +1,3 @@
+<div class="json-view" style="white-space: pre-wrap; font-family: 'Courier New', Courier, monospace"><?php
+    echo json_encode(json_decode($value,true),JSON_PRETTY_PRINT);
+?></div>
