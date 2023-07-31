944e51e308d1fbec8babd8676b84ad4b70bc1f9a
Francisco <franciscoigor@gmail.com>
2023-07-30 18:03:26 -0700

    fix user notification

--- app/Models/UserNotifications.php
+++ app/Models/UserNotifications.php
@@ -102,8 +102,6 @@ class UserNotifications extends BaseModel
         if ($user['push_token']){
             helper('pushnotifications');
             $result = @push_notification($user['push_token'],$notif['title'],strip_tags($notif['content']),['link'=>$notif['link']]);
-            print_r($result);
-            die();
         }
         return $id;
     }
