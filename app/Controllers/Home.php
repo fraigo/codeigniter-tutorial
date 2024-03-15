38fe9245af37fb3540eb9e80bdf79b866fe8725f
Francisco <franciscoigor@gmail.com>
2024-03-15 15:44:34 -0700

    Setup private/custom home page

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,8 +6,17 @@ class Home extends BaseController
 {
     public function index()
     {
+        helper("auth");
+        if (getenv('app.private')=="true" && !user_id()) {
+            http_response_code(getenv('app.private_response')?:404);
+            die();    
+        }    
         $pages = new \App\Models\Pages();
-        $page = $pages->find(1);
+        $page = $pages->find(getenv('app.homepage')?:1);
+        if (!$page){
+            http_response_code(404);
+            die(); 
+        }
         return $this->layout('page',$page);
     }
 
