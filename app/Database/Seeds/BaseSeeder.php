344e5d0c8b4df41b4f91c70d09fbc1ed00bb91c2
Francisco <franciscoigor@gmail.com>
2024-04-17 20:16:03 -0700

    Seeder progress + totals

--- app/Database/Seeds/BaseSeeder.php
+++ app/Database/Seeds/BaseSeeder.php
@@ -16,9 +16,15 @@ class BaseSeeder extends Seeder
             return;
         }
         echo "Importing $table\n";
+        $start = microtime(true);
         $lines = explode("\n",$data);
         $header = array_shift($lines);
         $fields = explode("\t",trim($header));
+        $total = 0;
+        $errors = 0;
+        $start = microtime(true);
+        $timer = microtime(true);
+        $count = count($lines) - 1;
         foreach($lines as $lineNum=>$line){
             if (trim($line)=="") continue;
             $values = explode("\t",trim($line));
@@ -44,9 +50,20 @@ class BaseSeeder extends Seeder
             } else {
                 $result = @$this->db->table($table)->insert($row);
             }
+            $total++;
+            $newtime = microtime(true);
+            if ($newtime-$timer>10){
+                $diff = round($newtime - $start);
+                $prec = round($total * 100 / $count);
+                echo "Imported $prec% $total/$count lines ($diff sec) ...\n";
+                $timer = $newtime;
+            }
             if (!$result){
+                $errors++;
                 echo "$table ".($lineNum+2).": ".$line."\n";
             }
         }
+        $duration = round((microtime(true) - $start)*100)/100;
+        echo "Duration: $duration seg. $total lines ($errors errors)\n";
     }
 }
