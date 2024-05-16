1af3ec723a8bf42d496cff5149d8f1934d52ce49
Francisco <franciscoigor@gmail.com>
2024-05-16 08:26:10 -0700

    start with .env server port

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,2 +1,7 @@
+PORT=$(grep -E "^app.baseURL" .env | grep "127.0.0.1" | grep -o -E ':[0-9]+' | grep -o -E "[0-9]+")
+if [ "$PORT" == "" ]; then
+    PORT=8080
+fi
+echo "Using port $PORT"
 rm -rf /writable/debugbar/*
-php spark serve --host 127.0.0.1
+php spark serve --host 127.0.0.1 --port $PORT
