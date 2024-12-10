07963bb9baa2550bb73666ba5e30ceb4a8ea4b1f
Francisco <franciscoigor@gmail.com>
2024-12-09 23:24:39 -0800

    Home page variables

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -10,13 +10,22 @@ class Home extends BaseController
         if (getenv('app.private')=="true" && !user_id()) {
             http_response_code(getenv('app.private_response')?:404);
             die();    
-        }    
+        }
         $pages = new \App\Models\Pages();
-        $page = $pages->find(getenv('app.homepage')?:1);
+        $pageId = getenv('app.homepage')?:1;
+        $page = $pages->find($pageId);
+        $userTypePage = $pages->where([
+            'slug'=>'home_page_'.current_user()['user_type']
+        ])->first();
+        if ($userTypePage){
+            $page = $userTypePage;
+        }
         if (!$page){
             http_response_code(404);
             die(); 
         }
+        $page['contents'] = str_replace('{today}',date('Y-m-d'),$page['contents']);
+        $page['contents'] = str_replace('{yesterday}',date('Y-m-d',strtotime('-1 day')),$page['contents']);
         return $this->layout('page',$page);
     }
 
